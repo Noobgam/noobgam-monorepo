eae07d4698ba5c51bc3694b55d700e0dbe24f54b
990 <shessmaster12@gmail.com>
2023-11-19 15:45:04 +0100

    feat: remove shorts by force

--- tampermonkey/block_sites.js
+++ tampermonkey/block_sites.js
@@ -1,20 +1,48 @@
 // ==UserScript==
 // @name         Block sites
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Block sites
 // @author       Noobgam
 // @match        http*://www.youtube.com/*
 // @match        http*://www.youtu.be/*
 // @grant        GM_addStyle
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
+function stopYoutubeWindow() {
     let dt = new Date();
     if (dt.getHours() > 0 && dt.getHours() < 12) {
         window.stop();
-        return;
     }
+}
+
+function removeElementByQuery(query) {
+    // requires jquery
+    const element = $(query);
+    if (element) {
+        element.remove()
+    }
+}
+
+function deleteShorts() {
+    removeElementByQuery(`[title='Shorts']`);
+    removeElementByQuery(`[tab-title='Shorts']`);
+    removeElementByQuery(`#shorts-container`);
+}
+
+function handleYoutube() {
+    deleteShorts();
+    stopYoutubeWindow();
+}
+
+function youtubeInject() {
+    setInterval(handleYoutube, 200)
+}
+
+(function() {
+    'use strict';
+    handleYoutube();
+    youtubeInject();
 })();
