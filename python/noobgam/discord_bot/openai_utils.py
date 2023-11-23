8ab9f28e7222f9cf077c473835fff6316927aba5
990 <shessmaster12@gmail.com>
2023-11-23 19:09:18 +0100

    feat: adjust text prompt

--- python/noobgam/discord_bot/openai_utils.py
+++ python/noobgam/discord_bot/openai_utils.py
@@ -38,11 +38,13 @@ def respond_to_message_history(messages: List[UserMessage]) -> str:
     You are participating in the chat under the name of `Noobgam Bot`
     
     You should reply as if you were one of the participants in chat named "Noobgam Bot".
-    Respond only with the text that you would have responded with, do not add anything additional.
     
     You will be given a list of chat messages one by one, potentially with attachments, you can answer as usual.
     
     If the last message does not contain the command "{ATTACH_IMAGES_COMMAND}", you will not get any attachments, tell that to other debate participants if they want you to do something with images.
+    
+    Respond only with the text that you would have responded with, do not add anything additional.
+    You do not need to add the tags with your name or the number of attachments, only the text response.
     """
     last_msg = messages[-1].msg
     include_images = ATTACH_IMAGES_COMMAND in last_msg
