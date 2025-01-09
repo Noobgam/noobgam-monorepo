fb1d7236e623a12f936b62d1fc60ff36ca0ae291
Noobgam <shessmaster12@gmail.com>
2025-01-09 10:39:55 +0100

    chore: limit calendar scopes
    NOOB-27

--- python/noobgam/local_playground/calendar_things.py
+++ python/noobgam/local_playground/calendar_things.py
@@ -3,7 +3,10 @@ from googleapiclient.discovery import build
 
 if __name__ == "__main__":
     oauth2_client_secret_file = 'F:\\Downloads\\noobgam_life.json'
-    scopes = ['https://www.googleapis.com/auth/calendar']
+    scopes = [
+        'https://www.googleapis.com/auth/calendar.calendarlist.readonly',
+        'https://www.googleapis.com/auth/calendar.events.readonly'
+    ]
 
     flow = InstalledAppFlow.from_client_secrets_file(oauth2_client_secret_file, scopes)
     creds = flow.run_local_server(port=0)
