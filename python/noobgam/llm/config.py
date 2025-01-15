a3d7a63d42e63d8feb03284849a850411110651b
Noobgam <shessmaster12@gmail.com>
2025-01-15 19:00:25 +0100

    chore: move to o1-preview for anki cards

--- python/noobgam/llm/config.py
+++ python/noobgam/llm/config.py
@@ -1,7 +1,6 @@
 import os
 
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.messages import SystemMessage
 
@@ -9,28 +8,17 @@ from noobgam.llm.chain import ConversationHistoryChain
 from noobgam.llm.prompt.prompts import ANKI_SYSTEM_PROMPT
 
 
-def _get_openai_llm() -> ChatOpenAI:
-    org = os.getenv("OPENAI_ORGANIZATION")
-    api_key = os.getenv("OPENAI_API_KEY")
-    if not org or not api_key:
-        raise ValueError("openai misconfiguration")
-    return ChatOpenAI(
-        temperature=0.1,
-        openai_api_key=api_key,
-        model_name="gpt-4o-2024-08-06",
-    )
-
-
-def _get_anthropic_llm() -> ChatAnthropic:
-    return ChatAnthropic(
-        temperature=0.1,
-        model_name="claude-3-5-sonnet-20241022",
-        max_tokens_to_sample=4096,
-    )
-
-
 def get_anki_chain(added_prompt: str = "") -> ConversationHistoryChain:
-    llm = _get_anthropic_llm()
+    api_key = os.environ["OPENROUTER_API_KEY"]
+    common_params = {
+        "base_url": "https://openrouter.ai/api/v1",
+        "api_key": api_key,
+        "temperature": 1,
+    }
+    llm = ChatOpenAI(
+        **common_params,
+        model="openai/o1-preview",
+    )
     chat_history = InMemoryChatMessageHistory(messages=[
         SystemMessage(content=ANKI_SYSTEM_PROMPT + added_prompt),
     ])
