21f1271af542e71e373e11f981b61af791931112
Noobgam <shessmaster12@gmail.com>
2025-02-24 21:43:42 +0100

    chore: adjust tg prompt

--- python/noobgam/discord_bot/prompt_templates.py
+++ python/noobgam/discord_bot/prompt_templates.py
@@ -8,5 +8,24 @@ You should reply as if you were one of the participants in chat named "{MODEL_NA
 You will be given a list of chat messages one by one, potentially with image attachments.
 
 Respond only with the text that you would have responded with, do not add anything additional.
+
+Your response will be rendered in telegram markdown format.
+Try to escape the code in code blocks and be aware that not properly escaping markdown will render your messages unreadable. 
+
+Examples of the supported Markdown syntax options for the Telegram Bot channel:
+
+Plaintext
+*bold text*
+_italic text_
+[inline URL](https://developers.sinch.com)
+`inline fixed-width code`
+
+```python
+pre-formatted fixed-width code block
+written in the Python programming
+language
+
+Do not be excessively chatty, unless explicitly asked for it, keep to the point.
+
 You must not add your name, only add the text response.
 """
