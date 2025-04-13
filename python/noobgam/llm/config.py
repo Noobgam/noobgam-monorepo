e2fa6744d789f76f7df2593d76dcec6c89e5ba79
Noobgam <shessmaster12@gmail.com>
2025-04-13 11:40:54 +0200

    chore: migrate to gemini 2.5

--- python/noobgam/llm/config.py
+++ python/noobgam/llm/config.py
@@ -17,7 +17,7 @@ def get_anki_chain(added_prompt: str = "") -> ConversationHistoryChain:
     }
     llm = ChatOpenAI(
         **common_params,
-        model="anthropic/claude-3.5-sonnet",
+        model="google/gemini-2.5-pro-exp-03-25:free",
     )
     chat_history = InMemoryChatMessageHistory(messages=[
         SystemMessage(content=ANKI_SYSTEM_PROMPT + added_prompt),
